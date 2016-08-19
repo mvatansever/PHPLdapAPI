23b7a73f0c61c6f0f4996e527ab39a84a2cd195d
Mesut Vatansever <mesut.vatansever@freebirdairlines.com>
2016-08-19 17:28:49 +0300

    install.php copy config dist files to php files this script wille execute after composer install

---
+++ install.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Mesut Vatansever | mesut.vatansever[at]freebirdairlines.com
+ * Date: 19/08/16 17:24
+ */
+
+copy('config/auth.php.dist', 'config/auth.php');
+copy('config/connection.php.dist', 'config/connection.php');
+copy('config/settings.php.dist', 'config/settings.php');
